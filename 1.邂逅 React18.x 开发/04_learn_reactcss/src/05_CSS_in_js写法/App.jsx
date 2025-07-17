616c361f54ded29383749502f3756ec3ac2ab829
betterme998 <betterme998@163.com>
2025-07-17 17:15:41 +0800

    css-in-js

--- 1.邂逅 React18.x 开发/04_learn_reactcss/src/05_CSS_in_js写法/App.jsx
+++ 1.邂逅 React18.x 开发/04_learn_reactcss/src/05_CSS_in_js写法/App.jsx
@@ -1,16 +1,30 @@
 import React, { PureComponent } from "react";
 // 导入由styled-components生成的组件，用它代替div
-import { AppWrapper } from "./style";
+import { AppWrapper, SectionWrapper } from "./style";
 
 export class App extends PureComponent {
+  constructor() {
+    super();
+
+    this.state = {
+      size: 30,
+      color: "yellow",
+    };
+  }
   render() {
+    const { size, color } = this.state;
     return (
       // 可以对子元素进行样式控制
       <AppWrapper>
-        <div className="section">
+        {/* 因为是组件，所以可以传入参数 */}
+        <SectionWrapper size={size}>
           <h2 className="title">我是标题</h2>
           <p className="content">我是内容，哈哈哈</p>
-        </div>
+          {/* 动态修改样式 */}
+          <button onClick={(e) => this.setState({ color: "skyblue" })}>
+            修改颜色
+          </button>
+        </SectionWrapper>
         <div className="footer">
           <p>免责声明</p>
           <p>版权声明</p>
--- 1.邂逅 React18.x 开发/04_learn_reactcss/src/05_CSS_in_js写法/style.js
+++ 1.邂逅 React18.x 开发/04_learn_reactcss/src/05_CSS_in_js写法/style.js
@@ -14,13 +14,33 @@ import styled from "styled-components";
 
 // styled.div``返回一个组件,div
 // 可以针对这个组件所有子元素进行样式编写
-
+// 1.基本使用
 export const AppWrapper = styled.div`
-  .section {
-    border: 1px solid red;
-  }
-
   .footer {
     border: 1px solid orange;
+  }
+`;
+
+// 2.子元素单独抽取到一个样式组件
+// 3.可以接受外部传染的props参数
+// 4.attrs:设置默认值，如果没有传入props参数，则使用默认值
+export const SectionWrapper = styled.div.attrs({
+  tColor: ((props) => props.color) || "blue",
+})`
+  border: 1px solid red;
+
+  .title {
+    color: ${(props) => props.tColor};
+    /* 获取外部参数props.size */
+    font-size: ${(props) => props.size}px;
+
+    &:hover {
+      background-color: purple;
+    }
+  }
+
+  .content {
+    color: green;
+    font-size: 20px;
   }
 `;
