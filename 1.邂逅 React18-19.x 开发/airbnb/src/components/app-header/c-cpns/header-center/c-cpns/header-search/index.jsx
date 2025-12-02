88b382913ec9e03c09e35f19e27db2561d6f9f08
betterme998 <betterme998@163.com>
2025-12-02 17:06:56 +0800

    搜索组件

--- 1.邂逅 React18-19.x 开发/airbnb/src/components/app-header/c-cpns/header-center/c-cpns/header-search/index.jsx
+++ 1.邂逅 React18-19.x 开发/airbnb/src/components/app-header/c-cpns/header-center/c-cpns/header-search/index.jsx
@@ -1,48 +1,33 @@
 import React, { memo, useState } from "react";
 import { SearchWarpper } from "./style";
-import { Radio } from "antd";
-const plainOptions = ["Apple", "Pear", "Orange", "Banana"];
+import { Segmented, ConfigProvider } from "antd";
 
 const HeaderSearch = memo(() => {
   const [navIndex, setNavIndex] = useState(0); //状态控制导航指示器位置
 
-  const handleNavChange = ({ target: { value } }) => {
-    setNavIndex(value);
-    console.log("radio1 checked", value);
-  };
   const handleLiChange = (index) => {
     setNavIndex(index);
   };
   return (
-    <SearchWarpper $activeIndex={navIndex}>
-      <div className="Shadow">
-        <div className="Shadow-white"></div>
-        <div className="Shadow-black"></div>
-      </div>
-      {/* <Radio.Group
-        name="radiogroup"
-        value={navIndex} //绑定到状态
-        onChange={handleNavChange} //添加change事件
-        options={[
-          { value: 0, label: "A" },
-          { value: 1, label: "B" },
-          { value: 2, label: "C" },
-          { value: 3, label: "D" },
-        ]}
-        optionType="button"
-      /> */}
-
-      <nav>
-        <ul>
-          {plainOptions.map((item, index) => {
-            return (
-              <li onClick={() => handleLiChange(index)}>
-                <label htmlFor="radiogroup">{item}</label>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+    <SearchWarpper className="headerSegmented" $activeIndex={navIndex}>
+      <ConfigProvider
+        theme={{
+          components: {
+            Segmented: {
+              block: true,
+            },
+          },
+        }}
+      >
+        <Segmented
+          options={["Daily", "Weekly", "Monthly", "Quarterly", "Yearly"]}
+          size="large"
+          shape="round"
+          onChange={(value) => {
+            console.log(value); // string
+          }}
+        />
+      </ConfigProvider>
     </SearchWarpper>
   );
 });
